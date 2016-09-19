ffab362c84fd5cf167af9afa5a01020f7e8680ff
Juan Padilla <jxpadilla@yahoo.com>
2016-09-19 17:35:46 -0500

    fixed the problem of writing/reading to Firebase

--- trainSchedule3.js
+++ trainSchedule3.js
@@ -6,115 +6,123 @@
 4. Create a way to calculate the next arrival and minutes away time. Using difference between start and current time. Then use moment.js formatting to set difference in minutes
 */
 // 1. Link to Firebase
-  // Initialize Firebase
   var config = {
     apiKey: "AIzaSyBYZF9LQJ0oeZrwsbd_buYJK6MUK1KHSHg",
     authDomain: "stfirebaseproject-4d75d.firebaseapp.com",
     databaseURL: "https://stfirebaseproject-4d75d.firebaseio.com",
     storageBucket: "stfirebaseproject-4d75d.appspot.com",
   };
+
+  // Initialize Firebase
   firebase.initializeApp(config);
 
 // 1. Link to Firebase
 // var trainData = new Firebase("https://stfirebaseproject-4d75d.firebaseio.com/");
 
-var database = firebase.database();
-var trainName, destiName, trainStart, freqMin;
-// 2. Button for adding trains
+	var database = firebase.database();
+
+// Variables
+	// Update HTML table with user inputs & initial time related values
+	var tName;
+	var tDesti;
+	var tFreq = 10;
+	var firstTime = "3:30";
+
+	// First Time (pushed back 1 year to make sure it comes before current time)
+	var firstTimeConverted = moment(firstTime,"hh:mm").subtract(1, "years");
+	console.log(firstTimeConverted);
+
+	// Current Time
+	var currentTime = moment().format ("LT");
+	// console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+	// Difference between the times
+	var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+	console.log("DIFFERENCE IN TIME: " + diffTime);
+
+	// Time apart (remainder)
+	var tRemainder = diffTime % tFreq;
+	console.log(tRemainder);
+
+	// Minute Until Train
+	var tMinutesTillTrain = tFreq - tRemainder;
+	console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+	// Next Train
+	var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+	console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+
+// 2. Functions
+//    Button for adding trains
 $("#addTrainBtn").on("click", function(){
 
-	// Get user input
-	var trainName = $("#inputTrain").val().trim();
-	var destiName = $("#inputDestination").val().trim();
-	var trainStart = moment($("#inputStartTime").val().trim().format("hh:mm:ss"));
-	var freqMin = $("#inputFrequency").val().trim();
+	// Get user inputs
+	tName = $("#inputTrain").val().trim();
+	tDesti = $("#inputDestination").val().trim();
+	firstTime =  "12:00"; //moment($("#inputStartTime").val().trim());
+	tFreq = $("#inputFrequency").val().trim();
 
 	// Create local "temporary" object for holding train data
 	var newTrain = {
-		name:  trainName,
-		destination: destiName,
-		start: trainStart,
-		frequency: freqMin
-		// created: firebase.database.ServerValue.TIMESTAMP,
-		// modified: firebase.database.ServerValue.TIMESTAMP
+		train:  tName,
+		destination: tDesti,
+		time: firstTime,
+		frequency: tFreq,
 	};
 
-	$(#inputTrain).html(trainName);
-	$(#inputDestination).html(destiName);	
-	$(#trainStartTime).html(trainStart);	
-	$(#inputFrequency).html(freqMin);
+	// $("#inputTrain").html(tName);
+	// $("#inputDestination").html(tDesti);	
+	// $("#trainStartTime").html(firstTime);	
+	// $("#inputFrequency").html(tFreq);
 
 
 
 	// Upload train data to the database
-	firebase.database.().ref().push(newTrain);
+	firebase.database().ref().push(newTrain);
 
 	// Logs everything to console
-	console.log(newTrain.name);
+	console.log(newTrain.train);
 	console.log(newTrain.destination);
-	console.log(newTrain.start);
+	console.log(newTrain.time);
 	console.log(newTrain.frequency);
-
-	// Alert
-	alert("Train successfully added");	
 
 	// Clears all of the text-boxes
 	$("#inputTrain").val("");
 	$("#inputDestination").val("");
 	$("#inputStartTime").val("");
 	$("#inputFrequency").val("");
-
-	// Determine when the next train arrives.
-	return false;	
-
 });
-
 
 // 3. Create Firebase event for adding train to the database and a row in the html when a user adds an entry
 database.ref().on("child_added", function(childSnapshot, prevChildKey){
 
-	console.log(childSnapshot.val());
+	console.log("childSnapshot=",childSnapshot.val());
 
 	// Store everything into a variable.
-	var trainName = childSnapshot.val().name;
-	var destiName = childSnapshot.val().destination;
-	var trainStart = childSnapshot.val().start;
-	var freqMin = childSnapshot.val().frequency;
+	var tName = childSnapshot.val().train;
+	var tDesti = childSnapshot.val().destination;
+	var firstTime = childSnapshot.val().time;
+	var tFreq = childSnapshot.val().frequency;
 
 	// Train Info
-	console.log(trainName);
-	console.log(destiName);
-	console.log(trainStart);
-	console.log(freqMin);
+	console.log(tName);
+	console.log(tDesti);
+	console.log(firstTime);
+	console.log(tFreq);
 
-	// Prettify the train start
-	// To calculate the next arrival
- //  var todaysDate = moment();
-	// var nextArrival = todaysDate.diff(oldStart, "minutes");
-	// console.log('Minutes: ' + trainMinutes);
-
-	// // To calculate the minutes away
-	// var minutesAway = trainMinutes * freqMin;
-	// console.log(minutesAway);
-
-	// // Add each train's data into the table
-
-	var newTrain = "<td>" + trainName + "</td>" + "<td>" + destiName + "</td>" + "<td>" + trainStart + "</td>" + "<td>" + freqMin + "</td>" + "<td>" + trainMinutes + "</td>"
+	var trainRow = "<td>" + tName + "</td>" + "<td>" + tDesti + "</td>" + "<td>" + firstTime + "</td>" + "<td>" + tFreq + "</td>" + "<td>" + tMinutesTillTrain + "</td>";
 	// $("#trainTable > tbody").append("<tr><td>" + trainName + "</td><td>" + destiName + "</td><td>" + freqMin + "</td><td>" + nextArrival + "</td><td>" + minutesAway + "</td></tr>");
 
-
-	var newRow = $("<tr>").append(newTrain);
+	var newRow = $('<tr>').html(trainRow);
    	$("#trainTable").append(newRow);
-   	
-   	return false;
-
-});
 
 
-// Example Time Math
-// -----------------------------------------------------------------------------
-// Assume train start date of January 1, 2015
-// Assume current date is March 1, 2016
+   	database.ref("addTrainBtn").set(null);
 
-// We know that this is 15 months.
-// Now we will create code in moment.js to confirm that any attempt we use mets this test case
+});  //end database call
+	
+	
+
+
+
